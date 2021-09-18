845d8d3a6a10f9b61a7db5ce7eb4834b5d8d0466
Lulef <accounts@mail.de>
2021-09-18 22:05:28 +0200

    +gut gut

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # from time import sleep
 import os
+from collections import defaultdict
 import random
 
 from rich.traceback import install
-install()
-
 from rich.console import Console
+
+install()
 con = Console()
 
 dSpielerNamen = {
@@ -16,6 +17,34 @@ dSpielerNamen = {
 }
 
 bClearscreen = True
+field_free = '.'
+field_hole = 'Loch'
+sClick = 'KLICK'
+num_of_ones = 24
+num_of_twos = 8
+num_of_threes = 4
+num_of_clicks = 12
+num_of_positions = 28
+
+lones = [1] * num_of_ones
+ltwos = [2] * num_of_twos
+lthrees = [3] * num_of_threes
+lclicks = [sClick] * num_of_clicks
+lDeck_of_cards = []
+
+lHoles = [13, 25, 16, 23, 4, 21, 7, 19, 10, None]
+dBoard = dict(zip(range(1, num_of_positions + 1), [field_free] * num_of_positions))
+
+dColors = {
+  1: {'text': '[blue]', 'desc': '[bold][blue]BLAU[/]', 'taken': None},
+  2: {'text': '[red]', 'desc': '[bold][red]ROT[/]', 'taken': None},
+  3: {'text': '[green]', 'desc': '[bold][green]GRÜN[/]', 'taken': None},
+  4: {'text': '[yellow]', 'desc': '[bold][yellow]GELB[/]', 'taken': None},
+  5: {'text': '[magenta]', 'desc': '[bold][magenta]MAGENTA[/]', 'taken': None},
+}
+
+# Start
+num_of_players = 4  # int(input('Wieviele Spieler?:\t'))
 
 
 def pick_card():
@@ -32,31 +61,23 @@ def click():
     yield h
 
 
-def get_next_player(current: int, max_players: int):
+def get_next_player(current: int):  # , max_players: int):
   next_player = current + 1
-  if next_player > max_players:
+  if next_player > num_of_players:
     next_player = 1
   return next_player
 
 
 def new_player(iPlayer: int):
-  ...
+  return False, 0
 
 
 def get_target_field(pos, iSteps):
-  # print('get_target_field')
-  # print(f'{pos = } - Typ: {type(pos)}')
-  # print(f'{iSteps = }')
   lNext_20_fields = list(dBoard.values())[pos:pos + 20]
-  # print(f'{lNext_20_fields = }')
 
-  # print(f'{lNext_20_fields[:iSteps] = }')
   ifields_free = lNext_20_fields[:iSteps].count(field_free)
-  # print(f'{ifields_free = }')
   ifields_holes = lNext_20_fields[:iSteps].count(field_hole)
-  # print(f'{ifields_holes = }')
   ifields = ifields_free + ifields_holes
-  # print(f'{ifields = }')
 
   iMoreSteps = iSteps
   while ifields != iSteps:
@@ -66,71 +87,31 @@ def get_target_field(pos, iSteps):
     ifields = ifields_free + ifields_holes
 
   iResult = pos + iMoreSteps
-  print(f'von Position {pos} {iSteps} Schritte landet auf {pos + iMoreSteps = }')
-  input('weiter...')
+  print(f'von Position {pos} {iSteps} Schritte landet auf {iResult}')
   return iResult
 
 
 def move_player(iPlayer: int, iSteps: int):
-  # print('move_player')
-  # print(f'{iPlayer = }')
-  # print(f'{iSteps = }')
   iPos = dPlayers[iPlayer]['positionen'][0]
-  # print(f'{iPos = }')
   iTarget_field = get_target_field(iPos, iSteps)
-  # print(f'{iTarget_field = }')
+  if iTarget_field >= num_of_positions:
+    dPlayers[iPlayer]['positionen'][0] = num_of_positions
+    dBoard[iPos] = field_free
+    return True, num_of_positions
   sTarget = dBoard[iTarget_field]
-  # print(f'{sTarget = }')
 
-# Ziel ein Loch
+  # Ziel ein Loch
   if sTarget == field_hole:
     dPlayers[iPlayer]['positionen'][0] = 0
     dBoard[iPos] = field_free
     print(f'{field_hole}...')
-# Ziel ist frei
+  # Ziel ist frei
   elif sTarget == field_free:
     dPlayers[iPlayer]['positionen'][0] = iTarget_field
     dBoard[iPos] = field_free
     print(f'{field_free}')
 
-
-def move_player1(iPlayer: int, iSteps: int):
-  con.print(f"{dPlayers[iPlayer]['name']} geht {iSteps} vorwärts!")
-  last_pos = dPlayers[iPlayer]['positionen'][0]
-  iTarget = dPlayers[iPlayer]['positionen'][0] + iSteps
-  print(f'Zielposition {iTarget = }')
-
-  if iTarget >= num_of_positions:
-    con.print(f"[yellow]YEAH! - {dPlayers[iPlayer]['name']} - HAT GEWONNEN")
-    dPlayers[iPlayer]['positionen'][0] = num_of_positions
-    dBoard[last_pos] = field_free
-    edit_board()
-    show_status_board()
-    exit()
-
-  # Zielposition ist Loch
-  if dBoard[iTarget] == field_hole:
-    dPlayers[iPlayer]['positionen'][0] = 0
-    dBoard[last_pos] = field_free
-    input(f'{field_hole}...')
-    return
-
-  # Zielposition schon besetzt
-  while True:
-    if dBoard[iTarget] != field_free and dBoard[iTarget] != field_hole:
-      iTarget += 1
-      if iTarget >= num_of_positions:
-        con.print(f"[yellow]YEAH! - {dPlayers[iPlayer]['name']} - HAT GEWONNEN")
-        dPlayers[iPlayer]['positionen'][0] = num_of_positions
-        dBoard[last_pos] = field_free
-        edit_board()
-        # show_status_board()
-        exit()
-    else:
-      break
-  if last_pos:
-    dBoard[last_pos] = field_free
-  dPlayers[iPlayer]['positionen'][0] = iTarget
+  return False, iTarget_field
 
 
 def show_status_board():
@@ -155,46 +136,18 @@ def edit_board():
         dBoard[pos] = v['name'][:7]
 
 
-field_free = '.'
-field_hole = 'Loch'
-num_of_ones = 24
-num_of_twos = 8
-num_of_threes = 4
-num_of_clicks = 12
-num_of_positions = 28
-
-lones = [1] * num_of_ones
-ltwos = [2] * num_of_twos
-lthrees = [3] * num_of_threes
-lclicks = ['click'] * num_of_clicks
-lDeck_of_cards = []
-
-lHoles = [13, 25, 16, 23, 4, 21, 7, 19, 10, None]
-dBoard = dict(zip(range(1, num_of_positions + 1), [field_free] * num_of_positions))
-
-dColors = {
-  1: {'desc': '[bold][blue]BLAU[/]', 'taken': None},
-  2: {'desc': '[bold][red]ROT[/]', 'taken': None},
-  3: {'desc': '[bold][green]GRÜN[/]', 'taken': None},
-  4: {'desc': '[bold][yellow]GELB[/]', 'taken': None},
-  5: {'desc': '[bold][magenta]MAGENTA[/]', 'taken': None},
-}
-
-# Start
-num_of_players = 4  # int(input('Wieviele Spieler?:\t'))
-
 dPlayers = {}
 for i in range(1, num_of_players + 1):
   name = dSpielerNamen[i]  # input(f'Wie heißt Spieler {i}?:\t')
   for k, dic in dColors.items():
     if dic['taken'] is None:
       con.print(f'{k}: {dic["desc"]}')
-  iColor = i  #int(input('Welche Farbe?: '))
+  iColor = i  # int(input('Welche Farbe?: '))
   dColors[iColor]['taken'] = i
   dPlayers[i] = {
     'name': name,
     'positionen': [0] * 4,
-    'color': dColors[iColor]['desc']
+    'color': dColors[iColor]['text']
   }
 
 for k, v in dPlayers.items():
@@ -203,22 +156,30 @@ for k, v in dPlayers.items():
 
 
 holes = click()
-curr_player = 1
+iCurr_player = 1
 hole = None
+dTimeline = {}
+iRound = 0
 
 while True:
-  # if bClearscreen:
-  #   os.system('clear')
+  if iCurr_player == 1:
+    iRound += 1
+    dTimeline[iRound] = []
+  if bClearscreen:
+    os.system('cls')
+  bWin, iTarget_field = False, None
+  sCard = None
+  sCurr_player = dPlayers[iCurr_player]['name']
+  sColor = dPlayers[iCurr_player]['color']
   edit_board()
   show_status_board()
-  con.print(f"[yellow]{dPlayers[curr_player]['name']}[/] zieht eine Karte:")
-  # sleep(0.1)
-  karte, lDeck_of_cards = pick_card()
-  con.print(f"{karte = }")
+  con.print(f"{sColor}{sCurr_player}[/] zieht eine Karte:")
+  sCard, lDeck_of_cards = pick_card()
+  con.print(f"{sCard = }")
 
-  if karte == 'click':
+  if sCard == sClick:
     last_hole = hole
-    con.print('[blue]Click!', end="")
+    con.print(f'[blue]{sClick}!', end="")
     hole = next(holes)
     if hole is None:
       holes = click()
@@ -229,15 +190,26 @@ while True:
     dBoard[hole] = field_hole
   else:
     # Player already present on board
-    if any(dPlayers[curr_player]['positionen']):
+    if any(dPlayers[iCurr_player]['positionen']):
       sNew = 'b'  # input('[N]eue Figur oder [B]ewegen?\t')[0].lower()
       if sNew == 'n':
-        new_player(curr_player)
+        bWin, iTarget_field = new_player(iCurr_player, sCard)
       else:
-        move_player(curr_player, karte)
+        bWin, iTarget_field = move_player(iCurr_player, sCard)
+        if bWin:
+          con.print(f'YEEAAHH!!!!! {sColor}{sCurr_player}[/] hat gewonnen!!')
     else:
       # Place first player on board
-      move_player(curr_player, karte)
+      bWin, iTarget_field = move_player(iCurr_player, sCard)
 
-  # sleep(1)
-  curr_player = get_next_player(curr_player, num_of_players)
+  turn = [iCurr_player, sCurr_player, sCard, iTarget_field]
+  dTimeline[iRound].append(turn)
+  if bWin:
+    break
+  iCurr_player = get_next_player(iCurr_player)  # , num_of_players)
+
+input('Ablauf...')
+for round, series in dTimeline.items():
+  con.print(f'\n[yellow]######### Runde {round} #########')
+  for iplayer, splayer, card, target in series:
+    con.print(f'{dPlayers[iplayer]["color"]}{splayer}[/] - {card} -> {target}')
